b82d66152db61e3f2ee57cd55dd3d12700670378
unknown <bhagavan.marolix@gmail.com>
2023-08-16 15:29:50 +0530

    assignment

---
+++ assignment14.py
@@ -0,0 +1,52 @@
+# n = ["praveen", 10, True, 15.876]
+# print(len(n))
+# lst = [10, 20, 30, 10, 40, 10, 50, 10, 60]
+# print(lst.count(10))
+# print(lst.count(2))
+
+# list1 = [1,2,3,4,5,6,7,8,9]
+# print(list1.index(4))
+
+# new_list = []
+# for item in range(10):
+#     new_list.append(item):
+# print(new_list)
+
+# list = [1,2,3,4,5]
+# print(id(list))
+
+# my_list = [[]] * 5
+# print(my_list)
+# my_list[1].append(7)
+# print(my_list)
+
+# my_list = [[]]*5
+# print(my_list)
+
+# list_1 = [1,2,3,4,5]
+# list_2 = [6,7,8,9,10]
+# new_list = list_1 + list_2
+# print(new_list)
+
+# list_1 = [1,2,23,34,46,45,87,56,90,100]
+# for num in list_1:
+#     if num % 2 == 0:
+#         print(num, end = " ")
+
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for num in list_1:
+#     if num % 2 == 1:
+#         print(num, end = " ")
+
+# list_1 = [10, 20, 30, 40, 50, 60]
+# list_2 = [1,2,3,4,5,6,7,8]
+# if list_1 == list_2:
+#     print(True) 
+# else:
+#     print(False)
+
+lst = [10, 20, 30, 40, 50, 60]
+lst.pop()
+print(lst)
+
+          
